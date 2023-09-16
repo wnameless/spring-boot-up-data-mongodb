1045f095912179bbecf561e40f392b882e569b6b
wmw <wnameless@gmail.com>
2023-09-16 13:39:47 +0800

    Trim test configs

--- src/test/java/cascade/github/AppConfig.java
+++ src/test/java/cascade/github/AppConfig.java
@@ -1,10 +1,8 @@
 package cascade.github;
 
 import org.springframework.context.annotation.Configuration;
-import com.github.wnameless.spring.boot.up.EnableSpringBootUp;
 import com.github.wnameless.spring.boot.up.data.mongodb.config.EnableSpringBootUpMongo;
 
 @EnableSpringBootUpMongo
-@EnableSpringBootUp
 @Configuration
 public class AppConfig {}
--- src/test/java/cascade/parent/AppConfig.java
+++ src/test/java/cascade/parent/AppConfig.java
@@ -16,10 +16,8 @@
 package cascade.parent;
 
 import org.springframework.context.annotation.Configuration;
-import com.github.wnameless.spring.boot.up.EnableSpringBootUp;
 import com.github.wnameless.spring.boot.up.data.mongodb.config.EnableSpringBootUpMongo;
 
 @EnableSpringBootUpMongo
-@EnableSpringBootUp
 @Configuration
 public class AppConfig {}
